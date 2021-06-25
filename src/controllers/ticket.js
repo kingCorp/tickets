3ed5808b13c097f0ac1a93756cb37cf4a6506bdb
Attasiem Kingsley <attasiemj@gmail.com>
2021-06-25 08:24:09 +0100

    fix: endpoint

--- src/controllers/ticket.js
+++ src/controllers/ticket.js
@@ -5,7 +5,7 @@ const Ticket = require('../models/ticket');
 
 //create ticket
 exports.createTicket = (req, res, next) => {
-  if (req.body.passcode !== 'agudareal') {
+  if (req.body.code !== 'agudareal') {
     return res.status(200).json({
       hasError: true,
       message: "Incorrect passcode cannot create ticket"
@@ -78,7 +78,7 @@ exports.getTickets = (req, res, next) => {
 exports.updateTicket = (req, res, next) => {
   const id = req.params.id;
 
-  if (req.body.passcode != 'agudareal') {
+  if (req.body.code != 'agudareal') {
     return res.status(200).json({
       hasError: true,
       message: "Incorrect passcode cannot create ticket"
